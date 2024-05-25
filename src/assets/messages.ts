f4c412fb274c4ccdd9e8b2f2a3ace61ea717e34b
FireIsGood <fireisadequate@gmail.com>
2024-05-25 01:37:40 -0700

    feat: more messages

--- src/assets/messages.ts
+++ src/assets/messages.ts
@@ -1,16 +1,37 @@
 const safe = [
   //
-  "Small changes",
-  "Fixed some stuff",
-  "Incredible work",
+  "small changes",
+  "fixed some stuff",
+  "fixed all the stuff",
+  "I wish this worked",
+  "incredible work",
   "sailKite is cute",
-  // Taken from the original
+  "oh yes",
+  "CI didn't pass but I changed the tests",
+  "it's so over",
+  "we're so back",
+  "whoops!",
+  "blame bit flips",
+  "WIP",
+  "# Please enter the commit message for your changes. Lines starting",
+  "Dear god please work",
+  "deleted the API key",
+  "good enough",
+  "forgot to add this last time",
+  "Not a backdoor",
+  "oopsies",
+  "Create LICENSE",
+  "Merge pull request #1 from NSA/5badb4",
+  "feat: h",
+  ".",
+  "-m",
   "--help",
-  "¯\\_(ツ)_/¯",
 ];
 const unsafe = [
   //
-  "For fuck's sake",
+  "Fuck it, we ball",
+  "What the fuck am I looking at",
+  "I don't get paid enough to deal with this shit",
 ];
 
 const all = [...safe, ...unsafe];
